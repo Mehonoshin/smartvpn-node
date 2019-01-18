c360149cabcf34364d1b830669a2fc01bc24949e
Stanislav Mekhonoshin <ejabberd@gmail.com>
2019-01-18 16:07:50 +0000

    Fix configuration to run server

--- bin/generate_keys.sh
+++ bin/generate_keys.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # This script should be run whenever we start the container with this image
 # for the first time on machine.
@@ -6,18 +6,22 @@
 # So the billing will be aware of them, and can email a proper connection configuration
 # to the SmartVPN user.
 
-make-cadir /hooks/pki
-echo "export KEY_NAME=\"EasyRSA\"" >> /hooks/pki/vars
-cd /hooks/pki
+if [ -d /hooks/pki ]
+then
+	make-cadir /hooks/pki
+	echo "export KEY_NAME=\"EasyRSA\"" >> /hooks/pki/vars
+	cd /hooks/pki
 
-source vars
+	ls -al
+	source ./vars
 
-./clean-all
-./build-ca --batch
-./build-key-server --batch server
-./build-dh
+	./clean-all
+	./build-ca --batch
+	./build-key-server --batch server
+	./build-dh
 
-openvpn --genkey --secret keys/ta.key
+	openvpn --genkey --secret keys/ta.key
 
-./build-key --batch generic_client
-
+	./build-key --batch generic_client
+	chmod 755 /etc/openvpn/server.up.sh
+fi
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,6 @@
 # TODO:
 # copy keys into image
 /hooks/bin/generate_keys.sh
+openvpn --config /etc/openvpn/server.conf
 #smartvpn activate
 
