60f3fbd6d22f80a8005b47a6197b8849db0dc84d
Stanislav Mekhonoshin <ejabberd@gmail.com>
2019-04-01 21:11:12 +0300

    Substitute env variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 # TODO: we need to substitute ENV variables in this script with those, that we
 # received from docker
-cp $HOOKS_PATH/bin/load_env.sh.template $HOOKS_PATH/bin/load_env.sh
+envsubst < $HOOKS_PATH/bin/load_env.sh.template > $HOOKS_PATH/bin/load_env.sh
 $HOOKS_PATH/bin/generate_keys.sh
 $HOOKS_PATH/bin/make_dev_tun.sh
 
